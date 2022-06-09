6449a4bef2fbaf0d275bd9447571d6ab0cbf4f61
Rapterian <68385686+Rapterian@users.noreply.github.com>
2022-06-09 14:44:07 +0200

    Added Product class

---
+++ FastFoodRestaurantMenu/FastFoodRestaurantMenu/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_2
+{
+    internal class Product
+    {
+        public string productName { get; set; }
+        public double productPrice { get; set; }
+        public Product(string productName, double productPrice)
+        {
+            this.productName = productName;
+            this.productPrice = productPrice;
+        }
+    }
+}
