65f3721b769acbec503a42f632a0f528e5ca129a
Rapterian <68385686+Rapterian@users.noreply.github.com>
2022-06-09 14:56:53 +0200

    Entering comments in the menu class to explain whats hapening

--- FastFoodRestaurantMenu/FastFoodRestaurantMenu/Menu.cs
+++ FastFoodRestaurantMenu/FastFoodRestaurantMenu/Menu.cs
@@ -8,6 +8,8 @@ namespace FastFoodRestaurantMenu
 {
     internal class Menu
     {
+        //the list will allow us to count and easaly loop through the items in the menu
+        //and to easally track them down if needed
         public List<string> items = new List<string>();
         public string menuHeading { get; set; }
         public string lastOption { get; set; }
@@ -15,13 +17,14 @@ namespace FastFoodRestaurantMenu
         public Menu(string menuHeading)
         {
             this.menuHeading = menuHeading;
-            lastOption = "Exit";
+            lastOption = "Exit";//making sure there is always an exit option is nice
         }
 
         public Menu(string menuHeading,string lastOption)
         {
             this.menuHeading = menuHeading;
-            this.lastOption = lastOption;
+            this.lastOption = lastOption;//giving the us the flexibility to change
+                                         //the last option to something like back
         }
 
         public void addItem(string item)
@@ -31,9 +34,10 @@ namespace FastFoodRestaurantMenu
 
         public void displayMenu()
         {
-            Console.WriteLine("\n"+menuHeading);
+            Console.WriteLine("\n"+menuHeading);//Leaving a line open before the menu
+                                                //makes it esier to read
             int count = 1;
-            foreach (string item in items)
+            foreach (string item in items)//automating the listing and counting saves time
             {
                 Console.Write(count++ + ".");
                 Console.WriteLine(item);
@@ -45,14 +49,14 @@ namespace FastFoodRestaurantMenu
         {
             Console.Write("Please type the number of the function you want to perform: ");
             string input = Console.ReadLine();
-            return checkInput(input, 1, items.Count + 1);
-
+            return checkInput(input, 1, items.Count + 1);//making sure the user can only give
+                                                         //a valid number
         }
 
         private static int checkInt(string input)
         {
-            while (!int.TryParse(input, out int result))
-            {
+            while (!int.TryParse(input, out int result))//keep asking for a numbe untill 
+            {                                           //one is given
                 Console.WriteLine("\n{0} is not a number, please enter a valid number.", input);
                 input = Console.ReadLine();
             }
@@ -64,14 +68,14 @@ namespace FastFoodRestaurantMenu
         {
             if (num > max) { return false; }
             if (num < min) { return false; }
-            return true;
+            return true;//making sure the user gives a number that is on the list
         }
 
         public static int checkInput(string input, int min, int max)
         {
             int num = checkInt(input);
-            while (!checkParameters(min, max, num))
-            {
+            while (!checkParameters(min, max, num))//keep asking the user for a number on the list
+            {                                      //untill one is given
                 Console.WriteLine("{0} is not on the list of numbers to choose from, please try again", num);
                 input = Console.ReadLine();
                 num = int.Parse(input);
