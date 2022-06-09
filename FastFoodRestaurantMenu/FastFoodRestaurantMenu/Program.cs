d11a5492ef68b66518555bf4af5478d20c4ef4be
Rapterian <68385686+Rapterian@users.noreply.github.com>
2022-06-09 14:48:40 +0200

    Added enums for all the menus Added a list for the cart that will hold the products

--- FastFoodRestaurantMenu/FastFoodRestaurantMenu/Program.cs
+++ FastFoodRestaurantMenu/FastFoodRestaurantMenu/Program.cs
@@ -1,9 +1,64 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace FastFoodRestaurantMenu
 {
     internal class Program
     {
+        enum MainMenu
+        {
+            Breakfast, Lunch, Drinks, CheckOut
+        }
+
+        enum BreakfastMenu
+        {
+            AllDayBrekkies, ToastedSandwiches
+        }
+
+        enum AllDayBrekkiesMenu
+        {
+            MzansiBrekkie, EarlyBird, StreakyBreakfast, Omelette, AvoOnToast
+        }
+
+        enum ToastedSandwichesMenu
+        {
+            Cheese, CheeseAndTomato, ChickenMayo, BaconAndEgg, Dagwood
+        }
+
+        enum LunchMenu
+        {
+            Burgers, FamousGrills
+        }
+
+        enum BurgerMenu
+        {
+            Wimpy, Cheese, Chicken, Champion
+        }
+
+        enum FamousGrillsMenu
+        {
+            ChickenAndChips, GrilledChickenFillets, ThrillOfTheGrill, ChickenWings
+        }
+        enum DrinksMenu
+        {
+            FrozenLemonades, Milkshake, HotDrinks
+        }
+
+        enum FrozenLemonadesMenu
+        {
+            PassionFruit, PassionFruitJug, Naartjie, NaartjieJug
+        }
+
+        enum MilkshakeMenu
+        {
+            Classic, BarOne, MilkTart, ToffeMocha
+        }
+        enum HotDrinksMenu
+        {
+            Cappachino, HotChocolate, Coffee, Tea
+        }
+
+        static List<Product> cart = new List<Product>();
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
